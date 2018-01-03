a92ca06cd9f5d35fab9ae6101e29e6e469db0b0f
Aaron Schreck <schreck.aaron@gmail.com>
2018-01-03 15:38:07 -0700

    add current_user method

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,6 +1,10 @@
 class DashboardController < ApplicationController
   
   def index
-    @user = User.find(session[:user_id])
+    if current_user
+      @user = current_user
+    else 
+      redirect_to '/signup'
+    end 
   end
 end 
