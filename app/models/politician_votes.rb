76157b662e1486b65ec5456527befbe3ffec1add
Aaron Schreck <schreck.aaron@gmail.com>
2018-01-06 12:25:43 -0700

    Refactor service and API calls out of the controller

--- app/models/politician_votes.rb
+++ app/models/politician_votes.rb
@@ -6,4 +6,23 @@ class PoliticianVotes
     @politician = politician
     @votes = []
   end 
+
+  def self.gather_votes_by_politician(politicians)
+    #for each politician, gather all of their votes
+    service = propublica
+      
+    all_politician_votes = politicians.flat_map do |politician|
+      all_votes = service.get_json("https://api.propublica.org/congress/v1/members/#{politician.uid}/votes.json")
+      pv = ::PoliticianVotes.new(politician.name)
+      pv.votes = all_votes[:results][0][:votes].map do |vote|
+        captured_vote = Vote.new(vote) 
+      end
+      pv
+    end
+  end
+
+
+    def self.propublica 
+      PropublicaService.new
+    end 
 end 
