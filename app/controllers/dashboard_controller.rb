b6fc3f896e184c9f14e5d403aeebfec358632b61
Aaron Schreck <schreck.aaron@gmail.com>
2018-01-06 12:53:36 -0700

    Wip refactor.

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -5,18 +5,6 @@ class DashboardController < ApplicationController
     if current_user
       @user = current_user
       @votes = PoliticianVotes.gather_votes_by_politician(@user.politicians) 
-
-
-
-      # service = PropublicaService.new
-      # @all_politician_votes = politicians.flat_map do |politician|
-      #   all_votes = service.get_json("https://api.propublica.org/congress/v1/members/#{politician.uid}/votes.json")
-      #   pv = ::PoliticianVotes.new(politician.name)
-      #   pv.votes = all_votes[:results][0][:votes].map do |vote|
-      #     captured_vote = Vote.new(vote) 
-      #   end
-      #   pv
-      # end
     else 
       redirect_to '/signup'
     end 
--- app/models/politician_votes.rb
+++ app/models/politician_votes.rb
@@ -10,14 +10,16 @@ class PoliticianVotes
   def self.gather_votes_by_politician(politicians)
     #for each politician, gather all of their votes
     service = propublica
-      
+    #all_politician_votes needs to be an array of PV objects  
     all_politician_votes = politicians.flat_map do |politician|
-      all_votes = service.get_json("https://api.propublica.org/congress/v1/members/#{politician.uid}/votes.json")
-      pv = ::PoliticianVotes.new(politician.name)
-      pv.votes = all_votes[:results][0][:votes].map do |vote|
-        captured_vote = Vote.new(vote) 
-      end
-      pv
+      service.get_politician_votes(politician)
+      
+      # all_votes = service.get_json("/congress/v1/members/#{politician.uid}/votes.json")
+      # pv = PoliticianVotes.new(politician.name)
+      # pv.votes = all_votes[:results][0][:votes].map do |vote|
+      #   captured_vote = Vote.new(vote) 
+      # end
+      # v
     end
   end
 
