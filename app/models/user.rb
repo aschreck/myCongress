187d616f8134b7a592a1e668b0b79ea14b9bb2ae
Aaron Schreck <schreck.aaron@gmail.com>
2018-01-07 10:03:03 -0700

    Add Validates, add minor graphical tweaks

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,6 @@ class User < ApplicationRecord
   has_secure_password
   has_many :followings
   has_many :politicians, through: :followings
+  validates_presence_of :name, :email, :password_digest
 
 end
