d0513df36f75c453f8bd9771780bd916197c715e
Aaron Schreck <schreck.aaron@gmail.com>
2018-01-10 10:04:13 -0700

    More experiment

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -7,7 +7,13 @@ Rails.application.config.assets.version = '1.0'
 # Rails.application.config.assets.paths << Emoji.images_path
 # Add Yarn node_modules folder to the asset load path.
 Rails.application.config.assets.paths << Rails.root.join('node_modules')
-Rails.application.config.assets.precompile << Rails.root.join("vendor", "assets", "javascripts", "fusioncharts")
+#Rails.application.config.assets.precompile << Rails.root.join("vendor", "assets", "javascripts", "fusioncharts")
+Dir[ Rails.root.join('vendor','assets', 'javascripts','*/') ].each do |dir|
+  dir = Pathname.new(dir)
+  Rails.application.config.assets.precompile += Dir[ dir.join('**','*') ].select{ |f| File.file?(f) }.map{ |f|                      
+  Pathname.new(f).relative_path_from(dir).to_s }
+end
+
 
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in the app/assets
