5333c3b9118344d2deceaf182b3b7bf67b9a3f56
Aaron Schreck <schreck.aaron@gmail.com>
2018-01-11 10:06:23 -0700

    Removed JS comments

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -35,7 +35,6 @@ $(document).ready(function () {
             <a href='/follow?name=${name}'>Follow</a> </br >
             </div>
           `)  
-          // $(".politicians").append(``)  
         });
       }
     })
